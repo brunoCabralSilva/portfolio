e2691c720c04326dc7e2cd6453fb994aaad8f473
Bruno <bruno.cabral.silva2018@gmail.com>
2024-12-19 15:39:18 -0300

    Mudança de icone do site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,16 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Portfólio",
   description: "Portfólio de Bruno Cabral - Desenvolvedor Fullstack",
+  openGraph: {
+    images: [
+      {
+        url: "https://media.licdn.com/dms/image/v2/D4D16AQGkz1V76eEzuA/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1698852550654?e=1740009600&v=beta&t=MuNe9y7culOznmyFJTBtyef8VA8BmtxvoreoRFHfRQU",
+        width: 1200,
+        height: 630,
+        alt: "Uma prévia do portfólio de Bruno Cabral.",
+      },
+    ],
+  }
 };
 
 export default function RootLayout({
